2655c39b183a5e9db853792bce277424edb79eb6
HyunSeung3681 <63762950+HyunSeung3681@users.noreply.github.com>
2020-05-25 14:09:28 +0900

    Update JuinE-Bot.py

--- JuinE-Bot.py
+++ JuinE-Bot.py
@@ -22,6 +22,11 @@ async def on_message(message) :
         return None
     if message.content == "쮜뉘야 도움":
         await message.channel.send("아직 만들고 있어요... 뚝딱뚝딱")
+    if message.content == "쮜뉘야 hellothisisverification":
+        if message.author.id == "285185716240252929":
+            await message.channel.send("현승 | HyunSeung#3681")
+        else:
+            await message.channel.send("죄송하지만 원더님만 사용할 수 있게 쮜뉘님이 설정해놓으셨어요... 현승 | HyunSeung#3681로 DM을 주시면 최대한 빨리 해놓을겁니다!")
 
 access_token=os.environ["BOT_TOKEN"]
 app.run(access_token)
